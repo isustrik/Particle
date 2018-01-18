42ac9a6b0a17da4ad4d567d1edae023a968fc18d
Ian A Sustrik <isustrik@users.noreply.github.com>
2018-01-18 13:58:13 -0700

    Create TankLevel_HC-204_DHT22_Ubidots.ino

---
+++ TankLevel_HC-204_DHT22_Ubidots.ino
@@ -0,0 +1,104 @@
+// These #include statement were automatically added by the Particle IDE.
+#include <Ubidots.h>
+#include <Adafruit_DHT.h>
+#include <HC_SR04.h>
+
+#define DHTPIN D6
+#define DHTTYPE DHT22
+#define trigPin D1
+#define echoPin D2
+#define TOKEN "TOKEN" //Enter your Ubidots Token here
+
+Ubidots ubidots(TOKEN);
+
+DHT dht(DHTPIN, DHTTYPE);
+
+double inches2water = 0.0;
+double tank = 0.0;
+double temp = 0.0;
+double hum = 0.0;
+int uploadminutes = 5; //Number of minutes to average before uploading 0 to 60; if less frequent the two arrays following must be increased in kind
+float tempArray[60]; //Hold temperature in Celcius
+float humArray[60]; //Holds humidity in % Relative Humidity
+int counter = 0;
+int avgcounter = 0; //Take the average Temperature and Humidity as well as a Water Tank Reading
+
+
+/*
+Connect an HC-SR04 Range finder as follows:
+Spark   HC-SR04
+GND     GND
+5V      VCC
+D0     Trig
+D1      Echo
+ 
+The default usable rangefinder is 10cm to 250cm. Outside of that range -1 is returned as the distance.
+ 
+You can change this range by supplying two extra parameters to the constructor of minCM and maxCM, like this:
+ 
+HC_SR04 rangefinder = HC_SR04(trigPin, echoPin, 5.0, 300.0);
+*/
+ 
+HC_SR04 rangefinder = HC_SR04(trigPin, echoPin);
+ 
+void setup() 
+{
+    Serial.begin(9600);
+    dht.begin();
+    delay(2000);
+    temp = dht.getTempCelcius(); //Reads in Celcius
+    hum = dht.getHumidity(); //Reads in %RH
+}
+ 
+void loop() 
+{
+    counter++;
+    if (counter==30) { //Every ~1min pull the numbers from the DHT Sensor package
+        environmentals();
+        counter = 0; //Reset the counter to start a new 30min count down
+    }
+    delay(2000);
+}
+
+void environmentals() { //Environmental Data Function
+    tempArray[avgcounter] = dht.getTempCelcius(); //Reads in Celcius
+    humArray[avgcounter] = dht.getHumidity(); //Reads in %RH
+    if (avgcounter==(uploadminutes - 1)) {
+        average();
+    } else {
+        avgcounter++;
+    }
+}
+
+void average() {
+    float tempavg = 0;
+    float humavg = 0;
+    for (int a = 0; a <= (uploadminutes - 1); a++){
+        tempavg = tempArray[a] + tempavg;
+        humavg = humArray[a] + humavg;
+    }
+    tempavg = tempavg/uploadminutes;
+    humavg = humavg/uploadminutes;
+    ubidots.add("WT_Temp",tempavg);
+    ubidots.add("WT_Humidity",humavg);
+    watertank();  //Take a water tank reading
+    ubidots.sendAll(); //Send all data at once to the Ubidot Cloud
+    avgcounter = 0;
+}
+
+void watertank() {
+    inches2water = rangefinder.getDistanceInch();
+    if (inches2water < 11.0) {
+        tank = 100.0;
+    }
+        else if (inches2water > 45.0) {
+            tank = 0.0;
+        }
+            else {
+                tank = 100-((inches2water/45)*100);
+            } 
+    ubidots.add("WT_Tank",tank);
+    ubidots.add("WT_Inches",inches2water);
+    Particle.variable("Tank", tank);
+    Particle.variable("I2W", inches2water);
+}
